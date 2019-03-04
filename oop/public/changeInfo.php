76451d5d8ecaa4b29e429a5525fb2919a13f40be
jyling <samuel.ling977@gmail.com>
2019-03-04 09:13:09 +0800

    Changed: changed the previous lname and fname to the nickname, old password, new password and  retyped password changed the validation to monitor the nickname, old password, new password and retyped password changed the updating code to update the nickname and the password of the user or just update the user's nickname changed the print error message to print for the new input nickname, old password and etc changed the input form to enable user to input the nickname, password and etc

--- oop/public/changeInfo.php
+++ oop/public/changeInfo.php
@@ -2,9 +2,12 @@
 require_once '../init.php';
 Page::addHead();
 Page::addNav();
-$fname = '';
-$lname = '';
+$nick = '';
+$pwd1 = '';
+$oldpwd = '';
+$repwd ='';
 $usr = new User();
+
 if (!$usr->getLogin()) {
   Page::redirect("index.php");
 }
@@ -12,41 +15,86 @@ else {
   if (Input::exist()) {
     $vali = new Validate();
     $result = $vali->check($_POST, array(
-      'fname' => array(
+      'nickname' => array(
         'required' => true,
-        'min' => 2,
-        'max' => 50,
+        'min' => 3,
+        'max' => 32,
         'charOnly' => true,
-        'name' => 'first name'
+        'name' => 'nickname'
       ) ,
-      'lname' => array(
+      'oldpass' => array(
         'required' => true,
         'min' => 2,
-        'max' => 50,
-        'charOnly' => true,
-        'name' => 'last name'
-      )
+        'max' => 64,
+        'name' => 'old password'
+      ),
+      'newpass' => array(
+        'min' => 6,
+        'max' => 64,
+        'charAndNums' => true,
+        'name' => 'new password',
+        'match' => 'repass',
+        'match_name' => 'second password field',
+      ),
+      'repass' => array(
+        'min' => 6,
+        'max' => 64,
+        'charAndNums' => true,
+        'name' => 'second password'
+      ),
     ));
     if ($vali->passed()) {
-      try {
-        $usr->update(array(
-          'fname' => Validate::sanitize(Input::get('fname')),
-          'lname' => Validate::sanitize(Input::get('lname'))
-        ));
-      } catch (\Exception $e) {
-        die('error' . $e);
+      $usr = new User();
+      $pwd = $usr->getData()->pwd;
+      if ($usr->verifyPass(Input::get('oldpass'),$pwd)) {
+        if(!
+            (
+              !Input::empty('nickname') &&
+                (
+                  Input::empty('newpass') &&
+                  Input::empty('repass')
+                  )
+                )
+            ){
+          echo 'name';
+          try {
+            $usr->update(array(
+              'nickname' => Validate::sanitize(Input::get('nickname'))));
+            } catch (\Exception $e) {
+              die('error' . $e);
+            }
+        }
+        elseif ((!Input::empty('newpass') && !Input::empty('repass'))) {
+          echo "name and pass";
+          try {
+          $usr->update(array(
+            'nickname' => Validate::sanitize(Input::get('nickname')),
+            'pwd' => PassHasher::getHash(Validate::sanitize(Input::get('password',true)))
+          ));
+          } catch (\Exception $e) {
+            die('error' . $e);
+          }
+        }
+        
       }
+          
 
     }
     else {
       foreach ($result->getError() as $key => $val) {
         $a = '<button type="button" class="close" data-dismiss="alert">&times;</button>';
         switch ($key) {
-          case 'fname':
-            $fname .= "<div class='thin-alert alert alert-danger alert-dismissible fade show'>$val $a</div>";
+          case 'nickname':
+            $nick .= "<div class='thin-alert alert alert-danger alert-dismissible fade show'>$val $a</div>";
               break;
-          case 'lname':
-            $lname .= "<div class='thin-alert alert alert-danger alert-dismissible fade show'>$val $a</div>";
+          case 'oldpass':
+            $oldpwd .= "<div class='thin-alert alert alert-danger alert-dismissible fade show'>$val $a</div>";
+              break;
+          case 'newpass':
+            $pwd1 .= "<div class='thin-alert alert alert-danger alert-dismissible fade show'>$val $a</div>";
+              break;
+          case 'repass':
+            $repwd .= "<div class='thin-alert alert alert-danger alert-dismissible fade show'>$val $a</div>";
               break;
           default:
             // code...
@@ -69,23 +117,23 @@ else {
       </div>
       <div class="form-group">
         <label for="usrname">Nickname</label>
-        <?php echo $fname ?>
-        <input class="form-control form-control" type="text" name="fname" id="fname" value="<?php echo Validate::sanitize($usr->getData()->fname) ?>" autocomplete="off">
+        <?php echo $nick ?>
+        <input class="form-control form-control" type="text" name="nickname" id="nickname" value="<?php echo Validate::sanitize($usr->getData()->nickname) ?>" autocomplete="off">
       </div>
       <div class="form-group">
         <label for="usrname">Old Password</label>
-        <?php echo $lname ?>
-        <input class="form-control form-control" type="text" name="lname" id="lname" value="<?php echo Validate::sanitize($usr->getData()->lname) ?>" autocomplete="off">
+        <?php echo $oldpwd ?>
+        <input class="form-control form-control" type="password" name="oldpass" id="oldpass" value="" autocomplete="off">
       </div>
       <div class="form-group">
         <label for="usrname">New Password</label>
-        <?php echo $lname ?>
-        <input class="form-control form-control" type="password" name="lname" id="lname" value="<?php echo Validate::sanitize($usr->getData()->lname) ?>" autocomplete="off">
+        <?php echo $pwd1 ?>
+        <input class="form-control form-control" type="password" name="newpass" id="newpass" value="" autocomplete="off">
       </div>
       <div class="form-group">
         <label for="usrname">Re-enter Password</label>
-        <?php echo $lname ?>
-        <input class="form-control form-control" type="password" name="lname" id="lname" value="<?php echo Validate::sanitize($usr->getData()->lname) ?>" autocomplete="off">
+        <?php echo $repwd ?>
+        <input class="form-control form-control" type="password" name="repass" id="repass" value="" autocomplete="off">
       </div>
       <?php
       Captcha::add();

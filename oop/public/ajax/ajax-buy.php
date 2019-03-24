ea4083601511cf4caa114c7fb43a5dbd08ca4d2a
jyling <samuel.ling977@gmail.com>
2019-03-24 12:12:52 +0800

    implemented the art purchase class

--- oop/public/ajax/ajax-buy.php
+++ oop/public/ajax/ajax-buy.php
@@ -1,5 +1,8 @@
 <?php
 require_once '../../init-nonpublic.php';
+// if (!defined('BASEPATH')) {
+//     exit(Page::redirect('../404.php'));
+// }
 
 $user = new User();
 if (!$user->getLogin()) {
@@ -12,9 +15,15 @@ $post = new Post(Input::get('post'));
 $msg  = $post->getData();
 $cost = $msg->cost;
 if ($coin->checkCoin() >= $cost) {
-    $coin->remove(Session::get('id'), $cost);
-    echo json_encode(array('success' => true, 'msg' => 'Post Has be post have been bought, thanks for supporting the artist'));
-    die();
+    $buy = new Purchase();
+    if ($buy->add($msg->post_id)) {
+        $coin->remove(Session::get('id'), $cost);
+        echo json_encode(array('success' => true, 'msg' => 'Post Has be post have been bought, thanks for supporting the artist'));
+        die();
+    } else {
+        echo json_encode(array('success' => false, 'msg' => 'an error has occured'));
+        die();
+    }
 } else {
     echo json_encode(array('success' => false, 'msg' => 'insufficient coins'));
     die();

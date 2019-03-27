0785772df41227148506cbb945e4a028f793e37c
jyling <samuel.ling977@gmail.com>
2019-03-27 10:56:47 +0800

    added function to provide download the image

--- oop/factory/Message.php
+++ oop/factory/Message.php
@@ -18,40 +18,87 @@ class Message
     {
         return substr($input, 0, $char - 3) . '...';
     }
-    public function generateMsg($template = 'messagebox.txt')
+    public function generateMsg($template = 'messagebox.txt', $con = true)
     {
         if (!empty($this->_output)) {
-            $msgs = $this->_output;
-            echo "<div class='card-deck'>";
-            foreach ($msgs as $msg) {
-                $message_id  = $msg->post_id;
-                $msg_content = (strlen($msg->content) > 50) ? self::StringOverflow($msg->content, 50) : $msg->content;
-                $user        = new User($msg->usr_id);
-                $fname       = $user->getData()->usrnm;
-                $name        = (strlen($fname) > 16) ? self::StringOverflow($fname) : $fname;
-                $title        = (strlen($msg->title) > 16) ? self::StringOverflow($msg->title) : $msg->title;
+            if ($con) {
+                $msgs = $this->_output;
+                echo "<div class='card-deck'>";
+                foreach ($msgs as $msg) {
+                    $message_id  = $msg->post_id;
+                    $msg_content = (strlen($msg->content) > 50) ? self::StringOverflow($msg->content, 50) : $msg->content;
+                    $user        = new User($msg->usr_id);
+                    $fname       = $user->getData()->usrnm;
+                    $name        = (strlen($fname) > 16) ? self::StringOverflow($fname) : $fname;
+                    $title       = (strlen($msg->title) > 16) ? self::StringOverflow($msg->title) : $msg->title;
 
-                $art         = Image::imgToBase64($msg->artThumbnail);
-                $read        = new Reader();
-                $read->read($template);
-                echo $read->modify(array(
-                    '$name'    => $name,
-                    '$fname'   => $fname,
-                    '$artPath' => $art,
-                    '$id'      => $message_id,
-                    '$post_id' => "viewPost.php?post=".$message_id,
-                    '$titleLong' => $msg->title,
-                    '$title' => $title,
-                    '$msg'     => $msg_content,
-                ));
-            }
+                    $art  = Image::imgToBase64($msg->artThumbnail);
+                    $read = new Reader();
+                    $read->read($template);
+                    echo $read->modify(array(
+                        '$name'      => $name,
+                        '$fname'     => $fname,
+                        '$artPath'   => $art,
+                        '$id'        => $message_id,
+                        '$post_id'   => "viewPost.php?post=" . $message_id,
+                        '$titleLong' => $msg->title,
+                        '$title'     => $title,
+                        '$msg'       => $msg_content,
+                    ));
+                }
+                echo "</div>";
+            } elseif (!$con) {
+                $msgs = $this->_output;
+                echo "<div class='card-deck'>";
+
+                foreach ($msgs as $purchase => $attr) {
+                    $author;
+                    $post_id;
+                    $msg;
+                    $title;
+                    $titleLong;
+
+                    // print_r($attr);
+                    if ($attr->post_id == null) {
+                        $author    = 'Deleted';
+                        $title     = 'Deleted';
+                        $titleLong = 'Deleted by artist or staff';
+
+                        $post_id = 'Deleted';
+                    } else {
+                        $post       = new Post($attr->post_id);
+                        $postDetail = $post->getData();
+                        $usr        = (new User($postDetail->usr_id))->getData();
+                        $author     = (strlen($usr->usrnm) > 16) ? self::StringOverflow($usr->usrnm) : $usr->usrnm;
+                        $msg        = (new Post($attr->post_id))->getData();
+                        $title      = (strlen($msg->title) > 16) ? self::StringOverflow($msg->title) : $msg->title;
+                        $titleLong  = $msg->title;
+                        $post_id    = $postDetail->post_id;
+                    }
+
+                    $button = "<a target='_blank' href='downloader.php?url=" . $attr->purchase_id . "'>Download</a>";
+                    $read   = new Reader();
+                    $read->read('messagebox.txt');
+                    // die(var_dump($post));
+                    echo $read->modify(array(
+                        '$name'      => $author,
+                        '$fname'     => $author,
+                        '$artPath'   => $attr->compressedImg,
+                        '$id'        => $post_id,
+                        '$post_id'   => "viewPost.php?post=" . $post_id,
+                        '$titleLong' => $title,
+                        '$title'     => $title,
+                        '$msg'       => $button,
+                    ));
+                }}
             echo "</div>";
         } else {
             echo "<h1 class='m-5 text-muted'>Welp, there's nothing here...</h1>";
         }
     }
-    public function totalPage($terms = array())
+
+    public function totalPage($table = 'post', $terms = array())
     {
-        return ceil(DB::Run()->get('post', $terms)->getCount() / $this->_limit);
+        return ceil(DB::Run()->get($table, $terms)->getCount() / $this->_limit);
     }
 }
